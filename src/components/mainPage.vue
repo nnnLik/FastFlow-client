6b7024c781498b6252fb50a4dffa1c1a5b82c7c7
nnnLik <makhmudov_ra@vk.com>
2023-03-11 23:34:52 +0300

    [ADD] new paragraph

---
+++ src/components/mainPage.vue
@@ -0,0 +1,149 @@
+<template>
+  <div>
+    <div style="background-color: #333; color: #fff; padding: 50px">
+      <div style="max-width: 800px; margin: 0 auto">
+        <div
+          style="
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: row-reverse;
+          "
+        >
+          <div style="flex: 1; padding-right: 40px">
+            <p style="font-size: 24px; font-weight: bold">FastFlow</p>
+            <p style="font-weight: normal">
+              FastFlow is an innovative open-source project that promises to
+              revolutionize the way we conduct video conferences.
+            </p>
+          </div>
+          <div style="flex: 1">
+            <img
+              src="path/to/image.jpg"
+              alt="FastFlow logo"
+              style="max-width: 100%"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div style="background-color: #444; color: #fff; padding: 50px">
+      <div style="max-width: 800px; margin: 0 auto">
+        <div
+          style="display: flex; align-items: center; justify-content: center"
+        >
+          <div style="flex: 1">
+            <img
+              src="path/to/image.jpg"
+              alt="FastFlow logo"
+              style="max-width: 100%"
+            />
+          </div>
+          <div style="flex: 1; padding-left: 40px">
+            <p style="font-size: 24px; font-weight: bold">
+              Unlimited Participants
+            </p>
+            <p style="font-weight: normal">
+              With FastFlow, you can communicate simultaneously to an unlimited
+              number of people in a video conference.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div style="background-color: #333; color: #fff; padding: 50px">
+      <div style="max-width: 800px; margin: 0 auto">
+        <div
+          style="
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: row-reverse;
+          "
+        >
+          <div style="flex: 1; padding-right: 40px">
+            <p style="font-size: 24px; font-weight: bold">Open Source</p>
+            <p style="font-weight: normal">
+              FastFlow is an open-source project under development, meaning that
+              anyone can contribute and help improve it.
+            </p>
+          </div>
+          <div style="flex: 1">
+            <img
+              src="path/to/image.jpg"
+              alt="FastFlow logo"
+              style="max-width: 100%"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div style="background-color: #444; color: #fff; padding: 50px">
+      <div style="max-width: 800px; margin: 0 auto">
+        <div
+          style="display: flex; align-items: center; justify-content: center"
+        >
+          <div style="flex: 1">
+            <img
+              src="path/to/image.jpg"
+              alt="FastFlow logo"
+              style="max-width: 100%"
+            />
+          </div>
+          <div style="flex: 1; padding-left: 40px">
+            <p style="font-size: 24px; font-weight: bold">
+              FastAPI, Vue, and Golang
+            </p>
+            <p style="font-weight: normal">
+              FastFlow is based on Python FastAPI, JavaScript Vue, and Golang
+              for video conferencing and messaging.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div style="background-color: #333; color: #fff; padding: 50px">
+      <div style="max-width: 800px; margin: 0 auto">
+        <div
+          style="
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: row-reverse;
+          "
+        >
+          <div style="flex: 1; padding-right: 40px">
+            <p style="font-size: 24px; font-weight: bold">Сonclusion</p>
+            <p style="font-weight: normal">
+              FastFlow is a highly efficient, scalable and easy-to-use video
+              conferencing platform for businesses of any size, equipped with
+              advanced technology and constant development.
+            </p>
+          </div>
+          <div style="flex: 1">
+            <img
+              src="path/to/image.jpg"
+              alt="FastFlow logo"
+              style="max-width: 100%"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+  import: "../assets/css/mainPage.css",
+};
+</script>
+
+<style scoped>
+@import "~bootstrap/dist/css/bootstrap.css";
+</style>
