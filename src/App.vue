2debe2b37f177d2c26fa8105af6252a8d3eaf760
nnnLik <makhmudov_ra@vk.com>
2023-03-11 23:33:38 +0300

    [ADD] main page

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+  </div>
+  <div>
+    <MainPage></MainPage>
+  </div>
+  <div>
+    <FooterMain></FooterMain>
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/navBar.vue";
+import MainPage from "./components/mainPage.vue";
+import FooterMain from "./components/footerMain.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+    MainPage,
+    FooterMain,
+  },
+};
+</script>
+
+<style scoped>
+@import "~bootstrap/dist/css/bootstrap.css";
+</style>
