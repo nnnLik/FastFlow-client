bdf86b478fe2a60223354a93a1ed48a3d4d12fc7
nnnLik <makhmudov_ra@vk.com>
2023-03-12 16:24:38 +0300

    [ADD] main wrap with route

---
+++ src/components/ff-main-wrapper.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="ff-main-wrapper">
+    <ffNavigateBar />
+    <router-view />
+    <ffFooter />
+  </div>
+</template>
+
+<script>
+import ffNavigateBar from "./layouts/ff-navigate-bar.vue";
+import ffFooter from "./layouts/ff-footer.vue";
+// import ffMainPage from "./main-page/ff-main-page.vue";
+
+export default {
+  name: "v-main-wrapper",
+  components: {
+    ffNavigateBar,
+    // ffMainPage,
+    ffFooter,
+  },
+  props: {},
+  data() {
+    return {
+      title: "Main wrapper",
+    };
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style>
+@import "~bootstrap/dist/css/bootstrap.css";
+</style>
